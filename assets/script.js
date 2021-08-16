a3781b193e656da799dde93de24d8cbcda27a223
Mohammad Zafar <sz7861@outlook.com>
2021-08-16 20:58:24 +0100

    Retrieve data to be displayed

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,14 @@ function getGridValue() {
   return getGrid + $.param(gridLocation);
 }
 
+// Retrieve data to be displayed
+function useWeatherApi(latValue, lonValue) {
+  var openW = "https://api.openweathermap.org/data/2.5/onecall?";
+  var getResponse = {
+    lat: latValue,
+    lon: lonValue,
+    appid: "35463baba77cb93af45f2339eaedb98d",
+  };
+  console.log(openW + $.param(getResponse));
+  return openW + $.param(getResponse);
+}
